8f87043e37bc1b1998994444394f01d75873a13d
achrafHaddad <achrafmail7@gmail.com>
2020-03-20 14:20:17 +0100

    multiple users

--- src/app/parent/formulaire/formulaire.component.ts
+++ src/app/parent/formulaire/formulaire.component.ts
@@ -1,36 +1,43 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { FormArray, Validators, FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-formulaire',
-  templateUrl: './formulaire.component.html',
-  styleUrls: ['./formulaire.component.css']
+  selector: "app-formulaire",
+  templateUrl: "./formulaire.component.html",
+  styleUrls: ["./formulaire.component.css"]
 })
 export class FormulaireComponent implements OnInit {
   formInfo: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  users = [];
 
-  get f(){
-    return this.formInfo.controls;
-  }
-  
+  constructor() {}
+
   @Output() public childEvent = new EventEmitter();
 
-  onSubmit(){
+  onSubmit() {
+    this.childEvent.emit(this.users);
+  }
+  addUser() {
     if (this.formInfo.invalid) {
-      return ;
+      return;
+    } else {
+      console.log(this.formInfo.value);
+      console.log(this.users);
+      this.users.push(this.formInfo.value);
+      this.formInfo.reset();
     }
-    this.childEvent.emit(this.formInfo.value)
   }
 
   ngOnInit() {
-    this.formInfo = this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      age: ['', [Validators.required, Validators.pattern("^[0-9]+$")]],
-      email: ['', [Validators.required, Validators.email]]
+    this.formInfo = new FormGroup({
+      firstName: new FormControl("", Validators.required),
+      lastName: new FormControl("", Validators.required),
+      age: new FormControl("", [
+        Validators.required,
+        Validators.pattern("^[0-9]+$")
+      ]),
+      email: new FormControl("", [Validators.required, Validators.email])
     });
   }
-
 }
